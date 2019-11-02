bf8262a76fd206f0387846fda6d04436952f9299
Monica Rondón <rondonmonica@johndeere.com>
2019-11-02 22:37:31 +0100

    Initial Commit

---
+++ PartyQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyQueue
+//
+//  Created by Rondon Monica on 02.11.19.
+//  Copyright © 2019 NotMoniApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
